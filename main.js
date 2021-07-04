03b53fd28859f8b911e1710484550dd0d47e7b76
rejkee <82302504+rejkee@users.noreply.github.com>
2021-07-04 22:26:48 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+(function ($) {
+    "use strict";
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.navbar').addClass('nav-sticky');
+        } else {
+            $('.navbar').removeClass('nav-sticky');
+        }
+    });
+
+})(jQuery);
+
